46aec7ba2a7d6e01d9ce5b8c9cf2658b4c7520ad
Guilherme Pedroza <gui_pedroza@hotmail.com>
2023-02-10 23:32:47 -0300

    MongoDB connection with repository and service

---
+++ src/main/java/com/pedroza/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.pedroza.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.pedroza.workshopmongo.domain.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
