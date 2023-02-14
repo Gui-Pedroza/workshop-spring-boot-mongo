d746cca9cbccd0635844d01e405c85a98369b0fe
Guilherme Pedroza <gui_pedroza@hotmail.com>
2023-02-14 08:24:45 -0300

    Delete and Update User

--- src/main/java/com/pedroza/workshopmongo/services/UserService.java
+++ src/main/java/com/pedroza/workshopmongo/services/UserService.java
@@ -30,8 +30,26 @@ public class UserService {
 		return userRepository.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepository.save(newObj);		
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setEmail(obj.getEmail());
+		newObj.setName(obj.getName());
+		
+	}
+
 	public User fromDTO(UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
+	 
 	
 }
