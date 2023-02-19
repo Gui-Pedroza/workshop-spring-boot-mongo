1713966706ed01681995b43c81291750f294bc6d
Guilherme Pedroza <gui_pedroza@hotmail.com>
2023-02-18 22:03:58 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/pedroza/workshopmongo/domain/Post.java
+++ src/main/java/com/pedroza/workshopmongo/domain/Post.java
@@ -2,11 +2,14 @@ package com.pedroza.workshopmongo.domain;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.pedroza.workshopmongo.dto.AuthorDTO;
+import com.pedroza.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -17,6 +20,7 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {		
 	}
@@ -60,6 +64,14 @@ public class Post implements Serializable {
 	public void setBody(String body) {
 		this.body = body;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -85,6 +97,8 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+
+	
 	
 	
 	
