e8f52e19440c9e4bc5016f99729981d7ba08d500
Guilherme Pedroza <gui_pedroza@hotmail.com>
2023-02-19 14:01:22 -0300

    Consulta simples com @Query

--- src/main/java/com/pedroza/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/pedroza/workshopmongo/repository/PostRepository.java
@@ -3,10 +3,14 @@ package com.pedroza.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.pedroza.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': {$regex: ?0, $options: 'i'}}")
+	List<Post> findByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
